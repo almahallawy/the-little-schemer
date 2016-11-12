3de2b993fcd8ee267603650cfe6209552eef731e
Khaled Almahallawy <almahallawy@gmail.com>
2016-11-11 23:55:42 -0800

    Fixing the eqan? function.

--- scheme/04-NumbersGames.rkt
+++ scheme/04-NumbersGames.rkt
@@ -157,7 +157,7 @@
     (cond
       ((and (number? a1) (number? a2))
        (o= a1 a2))
-      ((and (number? a1) (number? a2))
+      ((or (number? a1) (number? a2))
        #f)
       (else (eq? a1 a2)))))
 
